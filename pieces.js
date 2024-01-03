bafe2fea1943d0280683f8316aec1b2dd8df53cd
erudit aoue <philaoue@gmail.com>
2024-01-03 18:33:53 +0100

    mon site est dynamique

--- pieces.js
+++ pieces.js
@@ -36,35 +36,114 @@ function genererPieces(pieces) {
         pieceElement.appendChild(stockElement);
 
         sectionFiches.appendChild(pieceElement);
-
+        document.body.appendChild(sectionFiches);
     };
+    console.log("ça marche");
 };
 
+//premier affichage de la page 
+genererPieces(pieces);
 
 const boutonTrier = document.querySelector(".btn-trier");
 boutonTrier.addEventListener("click", function () {
     const piecesOrdonnees = Array.from(pieces);
     piecesOrdonnees.sort(function (a, b) {
-        return a.prix - b.prix;
+        return b.prix - a.prix;
     });
-    /**la methode sort est utilisée pour trier un tableau. Elle s'attend à recevoir une valeur de la fonction qu'elle contient. la fonction elle va faire le calcul suivant
-     * deuxième paramètre (b) - premier paramètre (a)
-     * si le résultat est positif alors le deuxième parammètre (b) est plus grand que le premier(a) et sera donc placé avant, 
-     * si le résultat est négatif alors le premier paramètre(a) est plus grand que le second(b), il sera placé avant.
-     * si le résultat est égal à 0, il n'y a pas de changements. 
-     */
-    console.log(piecesOrdonnees);
+   
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
 });
 
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 boutonFiltrer.addEventListener("click", () => {
 
     const piecesFiltres = pieces.filter(function (piece) {
-        return piece.prix <= 35;
+        return pieces.disponibilite;
     })
-    console.log(piecesFiltres);
+   //effacement de l'écran et regénération de la page avec pièces
+   document.querySelector(".fiches").innerHTML="";
+   genererPieces(piecesFiltres);
 });
 
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
+});
+
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
+});
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+console.log(noms)
+//Création de l'en-tête
+
+const pElement = document.createElement('p')
+pElement.innerText = "Pièces abordables";
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement);
+}
+
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(pElement)
+    .appendChild(abordablesElements);
+
+//Code Exercice 
+const nomsDisponibles = pieces.map(piece => piece.nom)
+const prixDisponibles = pieces.map(piece => piece.prix)
+
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`
+    disponiblesElement.appendChild(nomElement);
+}
+
+const pElementDisponible = document.createElement('p')
+pElementDisponible.innerText = "Pièces disponibles:";
+document.querySelector('.disponibles').appendChild(pElementDisponible).appendChild(disponiblesElement)
+
+const inputPrixMax = document.querySelector('#prix-max')
+inputPrixMax.addEventListener('input', function(){
+    const piecesFiltrees = pieces.filter(function(piece){
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);  
+})
+/*
 const noms = pieces.map(pieces => pieces.nom);
 /**la methode map s'applique sur un tableau d'origine et permet de créer un nouveau tableau contenant les éléments du tableau d'origine qui eux ont étés modifiés par une fonction prise en paramètre par map
  * 
@@ -75,7 +154,7 @@ const noms = pieces.map(pieces => pieces.nom);
  * La méthode map() crée un nouveau tableau contenant les résultats de l'application de la fonction à chaque élément.
  * Le tableau d'origine reste inchangé.
   */
-
+/*
 for (let i = pieces.length - 1; i >= 0; i--) {
 
     if (pieces[i].prix > 35) {
@@ -109,4 +188,4 @@ for (let i = 0; i < pieces.length; i++) {
 }
 
 const disponible = document.querySelector(".disponibles");
-disponible.appendChild(listeDispo);
+disponible.appendChild(listeDispo);*/
